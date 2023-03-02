97b31330e2d258e734b81a932b41435db47e98d7
Anthony Barreau <anthony.barreau@itk.fr>
2023-03-02 22:36:32 +0100

    Adds eslint config and updates code using new code style.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
 import './App.css';
-import UsersTable from "./components/UsersTable";
+import UsersTable from './components/UsersTable';
 
-function App() {
-  return (
-    <UsersTable/>
-  );
+function App () {
+    return (
+        <UsersTable/>
+    );
 }
 
 export default App;
