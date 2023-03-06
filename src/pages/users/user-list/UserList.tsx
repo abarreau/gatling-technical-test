5093994eb08b8ddff217f2cef8f96ef6b1783395
Anthony Barreau <anthony.barreau@itk.fr>
2023-03-06 18:18:50 +0100

    Adds dummy delay on users loading to display spinner

--- src/pages/users/user-list/UserList.tsx
+++ src/pages/users/user-list/UserList.tsx
@@ -3,12 +3,13 @@ import { usersToTableDatasource } from './UserList.mapper';
 import { Table, TableDatasource } from '../../../components/table/Table';
 import { Spinner } from '../../../components/spinner/Spinner';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchUsers, usersSelector } from '../../../redux/users-slice';
+import { fetchUsers, usersSelector, usersStoreStatusSelected } from '../../../redux/users-slice';
 import { useNavigate } from 'react-router-dom';
 
 export const UserList: FC = () => {
     const users = useSelector(usersSelector);
-    const [ dataSource, setDatasource ] = useState<TableDatasource>();
+    const userStateStatus = useSelector(usersStoreStatusSelected);
+    const [ dataSource, setDatasource ] = useState<TableDatasource | undefined>();
     const dispatch = useDispatch<any>();
     const navigate = useNavigate();
 
@@ -17,10 +18,12 @@ export const UserList: FC = () => {
     }, []);
 
     useEffect(() => {
-        if(users) {
+        if(userStateStatus === 'idle') {
             setDatasource(usersToTableDatasource(users, navigate));
+        } else {
+            setDatasource(undefined);
         }
-    }, [ users ]);
+    }, [ userStateStatus ]);
 
     if(!dataSource) {
         return (<Spinner/>);
